5643e6fb9762ddce2d4175de87c04b03f6c561c6
Manuel Kipfer <manuel.kipfer@swisscom.com>
2017-07-28 00:43:04 +0200

    - Add genre

--- src/js/lib/data/rest/DummyData.js
+++ src/js/lib/data/rest/DummyData.js
@@ -5,39 +5,45 @@ const data = {
       title: 'Samy Deluxe',
       description: 'Greatest Hits',
       thumb: 'https://vignette4.wikia.nocookie.net/powerrangers/images/8/81/Cover-powerrangersmovie.jpg/revision/latest/scale-to-width-down/660?cb=20161201090346',
-      count: 50000
+      count: 50000,
+      genre: 'rap'
     },
     find: [{
       _id: 2,
       title: 'Samy Deluxe',
       description: "Samy's greatest hits",
       thumb: 'http://rap.de/wp-content/uploads/2014/01/samy-deluxe.jpg',
-      count: 250
+      count: 250,
+      genre: 'rap'
     }, {
       _id: 3,
       title: 'Power Rangers',
       description: "Power Rangers Squad",
       thumb: 'https://vignette4.wikia.nocookie.net/powerrangers/images/8/81/Cover-powerrangersmovie.jpg/revision/latest/scale-to-width-down/660?cb=20161201090346',
-      count: 50000
+      count: 50000,
+      genre: 'geil'
     }],
     all: [{
       _id: 1,
       title: "Super Oesch's",
       description: "Super Oesch's greatest hits",
       thumb: 'https://www.enjoystmoritz.ch/wp-content/uploads/2017/03/Pressefoto6.jpg',
-      count: 25
+      count: 25,
+      genre: 'nice'
     }, {
       _id: 2,
       title: 'Samy Deluxe',
       description: "Samy's greatest hits",
       thumb: 'http://rap.de/wp-content/uploads/2014/01/samy-deluxe.jpg',
-      count: 250
+      count: 250,
+      genre: 'rap'
     }, {
       _id: 3,
       title: 'Power Rangers',
       description: "Power Rangers Squad",
       thumb: 'https://vignette4.wikia.nocookie.net/powerrangers/images/8/81/Cover-powerrangersmovie.jpg/revision/latest/scale-to-width-down/660?cb=20161201090346',
-      count: 50000
+      count: 50000,
+      genre: 'geil'
     }],
     recent: [{
       _id: 1,
--- src/js/pages/playlist/Overview.js
+++ src/js/pages/playlist/Overview.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { groupBy } from 'lodash';
 import { Grid, Row, Col, Tabs, Tab } from 'react-bootstrap';
 import { PlaylistGroup } from '../../components/playlist/Group';
 import { PlaylistItem } from '../../components/playlist/Item';
@@ -21,6 +22,7 @@ class PlaylistOverview extends React.Component {
   componentWillMount() {
     this.props.dispatch(PlaylistActions.execute('RECENT'));
     this.props.dispatch(PlaylistActions.execute('POPULAR'));
+    this.props.dispatch(PlaylistActions.execute('ALL'));
   }
 
   search(term) {
@@ -28,9 +30,15 @@ class PlaylistOverview extends React.Component {
   }
 
   render() {
-    const { recent, popular, find } = this.props.playlist;
+    const { recent, popular, find, all } = this.props.playlist;
     let { favorites } = this.props.user.get;
     favorites = favorites || [];
+
+    // Group by genre
+    const grouped = _.groupBy(all, 'genre');
+    const genres = _.map(grouped, (items, genre) => {
+      return <PlaylistGroup title={genre} playlists={items}></PlaylistGroup>;
+    });
 
     return(
       <Row>
@@ -41,7 +49,7 @@ class PlaylistOverview extends React.Component {
             <PlaylistGroup title="Recently Added" playlists={recent}></PlaylistGroup>
             <PlaylistGroup title="Most Popular" playlists={popular}></PlaylistGroup>
           </Tab>
-          <Tab eventKey={2} title="Genres">aasdfasdfs</Tab>
+          <Tab eventKey={2} title="Genres">{genres}</Tab>
           <Tab eventKey={3} title="Favorites">
             <PlaylistGroup title="Favorites" playlists={favorites}></PlaylistGroup>
           </Tab>
